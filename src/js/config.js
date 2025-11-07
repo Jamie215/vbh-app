f4a405fade96cf28fb2153ca035c5d856349350f
Jamie Kim <jmkim0603@gmail.com>
2025-11-07 11:04:19 -0500

    Thumbnail link updating

--- src/js/config.js
+++ src/js/config.js
@@ -13,7 +13,7 @@ const PLAYLISTS = [
         id: 'beginner-0-3',
         title: 'Beginner 0-3',
         description: 'Light load workout for starting',
-        thumbnail: 'https://img.youtube.com/vi/hq60J8wfNZY/maxresdefault.jpg',
+        thumbnail: 'https://img.youtube.com/vi/hq60J8wfNZY/hqdefault.jpg',
         videos: [
             {
                 id: 'hq60J8wfNZY',
@@ -21,7 +21,7 @@ const PLAYLISTS = [
                 sets: 3,
                 reps: 8,
                 equipment: ["Chair (Easier)", "No Chair (More Challenging)"],
-                thumbnail: 'https://img.youtube.com/vi/hq60J8wfNZY/maxresdefault.jpg',
+                thumbnail: 'https://img.youtube.com/vi/hq60J8wfNZY/hqdefault.jpg',
                 order: 1
             },
             {
@@ -30,7 +30,7 @@ const PLAYLISTS = [
                 sets: 3,
                 reps: 8,
                 equipment:["Chair (Easier)", "No Chair (More Challenging)", "Weight (Challenging)"],
-                thumbnail: 'https://img.youtube.com/vi/bcEDTtncUD0/maxresdefault.jpg',
+                thumbnail: 'https://img.youtube.com/vi/bcEDTtncUD0/hqdefault.jpg',
                 order: 2
             }
         ]
@@ -39,7 +39,7 @@ const PLAYLISTS = [
         id: 'intermediate-0-3',
         title: 'Intermediate 0-3',
         description: 'Intermediate load workout for more challenge',
-        thumbnail: 'https://img.youtube.com/vi/6a3qaXho5Q4/maxresdefault.jpg',
+        thumbnail: 'https://img.youtube.com/vi/6a3qaXho5Q4/hqdefault.jpg',
         videos: [
             {
                 id: '6a3qaXho5Q4',
@@ -47,7 +47,7 @@ const PLAYLISTS = [
                 sets: 3,
                 reps: 8,
                 equipment: ["Chair (Easier)", "No Chair (More Challenging)"],
-                thumbnail: 'https://img.youtube.com/vi/6a3qaXho5Q4/maxresdefault.jpg',
+                thumbnail: 'https://img.youtube.com/vi/6a3qaXho5Q4/hqdefault.jpg',
                 order: 1
             },
             {
@@ -56,7 +56,7 @@ const PLAYLISTS = [
                 sets: 3,
                 reps: 8,
                 equipment: ["Dowel (Easier)", "Weights (More Challenging)"],
-                thumbnail: 'https://img.youtube.com/vi/8RTh6Oh0weY/maxresdefault.jpg',
+                thumbnail: 'https://img.youtube.com/vi/8RTh6Oh0weY/hqdefault.jpg',
                 order: 2
             }
         ]
@@ -65,7 +65,7 @@ const PLAYLISTS = [
         id: 'advanced-0-3',
         title: 'Advanced 0-3',
         description: 'Advanced load workout for most challenge',
-        thumbnail: 'https://img.youtube.com/vi/gqzZ0ExlyMc/maxresdefault.jpg',
+        thumbnail: 'https://img.youtube.com/vi/gqzZ0ExlyMc/hqdefault.jpg',
         videos: [
             {
                 id: 'gqzZ0ExlyMc',
@@ -73,7 +73,7 @@ const PLAYLISTS = [
                 sets: 3,
                 reps: 8,
                 equipment: ["Resistance Band"],
-                thumbnail: 'https://img.youtube.com/vi/gqzZ0ExlyMc/maxresdefault.jpg',
+                thumbnail: 'https://img.youtube.com/vi/gqzZ0ExlyMc/hqdefault.jpg',
                 order: 1
             }
         ]
